dc82de501daffd92995027c33bc98e96c599be65
VikrantSingh16 <vickey016@>
2022-03-16 07:44:05 +0530

    User data page and Food Categogory

---
+++ app/src/main/java/com/example/donationapp/donorhomepage.java
@@ -0,0 +1,17 @@
+package com.example.donationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class donorhomepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donorhomepage);
+
+        //changing statusbar color
+        statusbar.blackiconstatusbar(donorhomepage.this,R.color.light_bg);
+    }
+}
